540363852c7c0bc0dec990c9f8765c3a74fd55bc
Roncz <roncz.oliver@students.jedlik.eu>
2024-04-09 20:25:21 +0200

    van kosár számlámó nincs megformázva #5

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ import { Bike } from './item.js'
 
 let shopButton = document.getElementById("toCart")
 let bikes = Bike.loadData(data)
+let cartCount = document.getElementById("count")
 let cart = []
+let count = 0
 
 document.getElementById('bikeContainer').addEventListener('click', function(event) {
     if (event.target.classList.contains('toCart')) {
@@ -15,7 +17,10 @@ function addToCart(button) {
     bikes.forEach(element => {
         if(element.id == button.value){
             cart.push(element)
+            count = cart.length
+            cartCount.innerHTML = count
         }
     });
     console.log(cart)
+    console.log(count)
 }
