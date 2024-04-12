de7b393ba139b06d85dfddd2cff34be0fd535b01
Roncz <roncz.oliver@students.jedlik.eu>
2024-04-12 22:38:22 +0200

    mosmár jó a végösszeg ha nem kér #8

--- configData.js
+++ configData.js
@@ -1,26 +1,30 @@
 //név;ár;kategória;verseny;path;id
 
 const configData = [
-        'null;0;null;0;;00',
 
+        'nemkérek;0;nemkérek;0;images/blank.png;00',
         'váz1;99990;váz;0;images/vaz_alap.png;01',
         'váz2;199990;váz;0;images/vaz_gravel.png;02',
         'váz3;349990;váz;0;images/vaz_race.png;03',
 
+        'nemkérek;0;nemkérek;0;images/blank.png;10',
         'kerék1;100;kerék;0;images/kerek_alap.png;11',
         'kerék1;100;kerék;0;images/kerek_sport.png;12',
         'kerék1;100;kerék;0;images/kerek_race.png;13',
 
+        'nemkérek;0;nemkérek;0;images/blank.png;20',
         'hajtás1;100;hajtás;0;images/hajtas_alap.png;21',
         'hajtás2;100;hajtás;0;images/kerek1.jpg;22',
         'hajtás3;100;hajtás;0;images/kerek1.jpg;23',
         'hajtás4;100;hajtás;0;images/kerek1.jpg;24',
 
+        'nemkérek;0;nemkérek;0;images/blank.png;30',
         'kormány1;100;kormány;0;images/kormany_alap.png;31',
         'kormány2;100;kormány;0;images/kerek1.jpg;32',
         'kormány3;100;kormány;0;images/kerek1.jpg;33',
         'kormány4;100;kormány;0;images/kerek1.jpg;34',
 
+        'nemkérek;0;nemkérek;0;images/blank.png;40',
         'nyereg1;100;nyereg;0;images/nyereg_alap.png;41',
         'nyereg2;100;nyereg;0;images/kerek1.jpg;42',
         'nyereg3;100;nyereg;0;images/kerek1.jpg;43',
