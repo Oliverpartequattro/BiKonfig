bc78863f5478dc6faa8a9330913209362afd8674
Roncz <roncz.oliver@students.jedlik.eu>
2024-04-15 20:15:15 +0200

    bolt oldal új formátum, képek ár stb #11

--- bikeData.js
+++ bikeData.js
@@ -1,12 +1,12 @@
 //név;év;ár;állapot;path;id
 
 const data = [
-    'Cippolini NK1K;2008;400;uj;asdasd;images/kerek1.jpg;1',
-    'TREK Madone SLR 9;2008;400;uj;asdasd;images/kerek1.jpg;2',
-    'Wilier Zero SLR;2008;400;uj;asdasd;images/kerek1.jpg;3',
-    'Canyon Aeroad CFR MVDP Edition;2008;400;uj;asdasd;images/kerek1.jpg;4',
-    'Bianchi Oltre XR4;2008;400;uj;asdasd;images/kerek1.jpg;5',
-    'Cannondale Topstone 3;2008;400;uj;asdasd;images/kerek1.jpg;6',
+    'Cipollini NK1K;2008;2390000;uj;asdasd;images/cipolliniNK1K.jpg;1',
+    'TREK Madone SLR 9;2008;2590000;uj;asdasd;images/trekmadoneslr9.jpg;2',
+    'Wilier Zero SLR;2008;2990000;uj;asdasd;images/willierzeroslr.png;3',
+    'Canyon Aeroad CFR MVDP Edition;2008;2790000;uj;asdasd;images/canyonaeroadcfrmvdp.png;4',
+    'Bianchi Oltre XR4;2008;2390000;uj;asdasd;images/bianchioltrexr4.jpg;5',
+    'Cannondale Topstone 3;2008;1190000;uj;asdasd;images/cannondaletopstone.jpg;6',
 ]
 
 export default data
--- createCard.js
+++ createCard.js
@@ -9,19 +9,21 @@ function createBikeCards() {
 
 bikes.forEach(bike => {
 
-        const cardDiv = document.createElement('div');
-        cardDiv.classList.add('col');
-        cardDiv.innerHTML = `
-            <div class="card h-100">
-                <img src="${bike.pathasd}" class="card-img-top" alt= "KEP">
-                <div class="card-body">
-                    <h5 class="card-title">${bike.name}</h5>
-                    <p class="card-title">${bike.description}</p>
-                    <p class="card-text">Ár: ${bike.price}</p>
-                    <button value="${bike.id}" class="btn toCart">Kosárba</button>
-                </div>
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('col');
+    cardDiv.innerHTML = `
+        <div class="card h-100">
+            <div class="d-flex justify-content-center align-items-center">
+                <img src="${bike.path}" class="shopimg" alt="KEP" width="40%" height="40%">
             </div>
-        `;
+            <div class="card-body text-center">
+                <h5 class="card-title">${bike.name}</h5>
+                <p class="card-title">${bike.description}</p>
+                <p class="card-text">Ár: ${bike.price.toLocaleString('hu-HU', { style: 'currency', currency: 'HUF' })}</p> 
+                <button value="${bike.id}" class="btn toCart">Kosárba</button>
+            </div>
+        </div>
+    `;
         container.appendChild(cardDiv);
     });
 }
